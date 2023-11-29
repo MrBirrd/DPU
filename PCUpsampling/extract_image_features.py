4ee3685c0b0b4c485979b0ff7dc5d635ee948939
Mathias <mathias.vogel.-nd@disneyresearch.com>
2023-11-29 10:07:26 +0100

    more efficient video loading and error tracing

--- PCUpsampling/extract_image_features.py
+++ PCUpsampling/extract_image_features.py
@@ -37,8 +37,7 @@ def main():
 
     # deactivate xformers
     os.environ["XFORMERS_DISABLED"] = "1"
-    
-    
+
     if args.scenes_files is not None:
         with open(args.scenes_files, "r") as f:
             scenes = f.read().splitlines()
@@ -48,9 +47,9 @@ def main():
 
     if args.output_dir is None:
         args.output_dir = args.data_root
-        
+
     print("Processing", len(scenes), "scenes")
-    
+
     for scene_id in scenes:
         # construct paths
         target_path = os.path.join(args.output_dir, scene_id, "features", f"{args.feature_type}")
@@ -73,7 +72,6 @@ def main():
             torch.cuda.empty_cache()
             gc.collect()
         except Exception as e:
-            print(e)
             print(traceback.format_exc())
             pass
 
