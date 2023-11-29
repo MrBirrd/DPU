0549f3eddead325d8db0743766fb0e11e5407892
Mathias <mathias.vogel.-nd@disneyresearch.com>
2023-11-29 14:41:13 +0100

    efficiency update for feature extraction

--- PCUpsampling/utils/image_features.py
+++ PCUpsampling/utils/image_features.py
@@ -34,37 +34,42 @@ def make_transform(smaller_edge_size: int) -> transforms.Compose:
     )
 
 
-def prepare_image(image, smaller_edge_size: float, patch_size: int) -> Tuple[torch.Tensor, Tuple[int, int]]:
+def prepare_image_batched(images, smaller_edge_size: float, patch_size: int) -> Tuple[torch.Tensor, Tuple[int, int]]:
     transform = make_transform(int(smaller_edge_size))
 
-    image_tensor = transform(image)
+    image_tensor = transform(images)
     # Crop image to dimensions that are a multiple of the patch size
-    height, width = image_tensor.shape[1:]  # C x H x W
+    height, width = image_tensor.shape[2:]  # B x C x H x W
     cropped_width, cropped_height = width - width % patch_size, height - height % patch_size
-    image_tensor = image_tensor[:, :cropped_height, :cropped_width]
+    image_tensor = image_tensor[:, :, :cropped_height, :cropped_width]
 
     grid_size = (cropped_height // patch_size, cropped_width // patch_size)  # h x w
     return image_tensor, grid_size
 
 
-def get_dino_features(model, image, patch_size=14):
-    C, H, W = image.shape
-    interpolation = transforms.Resize(size=(H, W), interpolation=transforms.InterpolationMode.BILINEAR, antialias=True)
-
-    smaller_edge_size = min(H, W)
-    image_tensor, grid_size = prepare_image(image, smaller_edge_size, patch_size)
-
-    with torch.inference_mode(), torch.cuda.amp.autocast():
-        image_batch = image_tensor.cuda().unsqueeze(0)
-        t = model.get_intermediate_layers(image_batch)[0].squeeze().cpu()
-
+def normalize_t(t):
     t_min = t.min(dim=0, keepdim=True).values
     t_max = t.max(dim=0, keepdim=True).values
     normalized_t = (t - t_min) / (t_max - t_min)
+    return normalized_t
 
-    features = normalized_t.reshape(*grid_size, -1)
-    features = rearrange(features, "h w c -> 1 c h w")
-    features = interpolation(features)
+
+@torch.no_grad()
+def get_dino_features(model, image, patch_size=14):
+    B, C, H, W = image.shape
+    smaller_edge_size = min(H, W)
+
+    image_batch, grid_size = prepare_image_batched(image, smaller_edge_size, patch_size)
+
+    t = model.get_intermediate_layers(image_batch)[0].squeeze().detach()
+
+    features = normalize_t(t).reshape(B, grid_size[0], grid_size[1], -1)
+    features = rearrange(features, "b h w c -> b c h w")
+
+    # features = torch.cat([torch.nn.functional.interpolate(f.unsqueeze(0), size=(H, W), mode="bilinear") for f in features])
+    features = torch.nn.functional.interpolate(
+        features, size=(H, W), mode="bilinear", recompute_scale_factor=False, antialias=False
+    )
     return features
 
 
@@ -79,43 +84,139 @@ def project_point_cloud(points, translation, rotation, camera_matrix):
     return points_projected.T
 
 
-def filter_points(points_projected, image_width, image_height, min_depth=0.1, max_depth=1000):
-    # Check within image bounds and positive depth
+def project_point_cloud_batch(points, translation, rotation, camera_matrix):
+    # Points: BxNx3
+    # Translation: Bx3x1
+    # Rotation: Bx3x3
+    # Camera Matrix: Bx3x3
+
+    # Apply rotation: Bx3x3 dot BxNx3 -> BxNx3
+    points_rotated = np.matmul(rotation, points.transpose(0, 2, 1))
+
+    # Apply translation: BxNx3 + Bx3x1 -> BxNx3
+    points_transformed = points_rotated + translation
+
+    # Apply camera matrix: Bx3x3 dot BxNx3 -> BxNx3
+    points_projected = np.matmul(camera_matrix, points_transformed)
+
+    # Normalize: Divide by the z-coordinate to project onto the image plane
+    points_projected = points_projected.transpose(0, 2, 1)
+    points_projected[..., :2] /= points_projected[..., 2:3]
+
+    return points_projected
+
+
+def filter_points_batch(points_projected, image_width, image_height, min_depth=0.1, max_depth=1000):
+    # points_projected is of shape BxNx3, where the last dimension is [x, y, depth]
+
+    # Check within image bounds for the entire batch
     in_image_bounds = (
-        (points_projected[:, 0] >= 0)
-        & (points_projected[:, 0] < image_width)
-        & (points_projected[:, 1] >= 0)
-        & (points_projected[:, 1] < image_height)
+        (points_projected[:, :, 0] >= 0)
+        & (points_projected[:, :, 0] < image_width)
+        & (points_projected[:, :, 1] >= 0)
+        & (points_projected[:, :, 1] < image_height)
     )
-    valid_depth = (points_projected[:, 2] > min_depth) & (points_projected[:, 2] < max_depth)
+
+    # Check valid depth for the entire batch
+    valid_depth = (points_projected[:, :, 2] > min_depth) & (points_projected[:, :, 2] < max_depth)
+
+    # Combine the conditions for the entire batch
     valid_indices = in_image_bounds & valid_depth
+
     return valid_indices
 
 
-def map_image_features_to_ptc(image, valid_points_2d, valid_indices):
-    # Assuming valid_points_2d is a NumPy array
-    x, y = valid_points_2d[:, 0].astype(int), valid_points_2d[:, 1].astype(int)
-    rgb_values = image[y, x]  # Efficient bulk operation
-    return rgb_values, valid_indices
+def map_image_features_to_filtered_ptc_batch(images, projected_points_batch, valid_indices_batch):
+    # images: BxHxWxC - a batch of images
+    # projected_points_batch: BxNx3 - a batch of projected 3D points
+    # valid_indices_batch: BxN - a batch of boolean valid indices
+
+    # Initialize a list to store the mapped features for each batch
+    mapped_features_batch = []
+
+    # Process each batch
+    for image, projected_points, valid_indices in zip(images, projected_points_batch, valid_indices_batch):
+        # Filter out valid 2D points using valid indices
+        valid_points_2d = projected_points[valid_indices, :2]
+
+        # Extract x and y coordinates, ensuring they are within the image bounds
+        x, y = valid_points_2d[:, 0].astype(int), valid_points_2d[:, 1].astype(int)
+        x = np.clip(x, 0, image.shape[1] - 1)
+        y = np.clip(y, 0, image.shape[0] - 1)
+
+        # Extract the features (e.g., RGB values) from the image at these coordinates
+        rgb_values = image[y, x]
+
+        # Append the features to the batch list
+        mapped_features_batch.append(rgb_values)
+
+    return mapped_features_batch
 
 
-def update_running_mean(current_mean, current_count, new_value):
-    new_count = current_count + 1
-    new_mean = current_mean + (new_value - current_mean) / new_count
-    return new_mean, new_count
+def update_features_batched(feature_array, count_array, new_features, valid_indices):
+    # feature_array: NxF - Pre-initialized feature array for running mean
+    # count_array: N - Pre-initialized array for counts
+    # new_features: BxMxF - Array of new features for each batch
+    # valid_indices: BxN - Boolean array of valid indices for each batch
+
+    for batch_new_features, batch_valid_indices in zip(new_features, valid_indices):
+        # Get indices where valid_indices is True
+        indices = np.where(batch_valid_indices)[0]
+
+        # Increment counts
+        count_array[indices] += 1
+
+        # Extract the current counts and features for valid indices
+        current_counts = count_array[indices]
+        current_means = feature_array[indices]
+
+        # Update the running mean
+        # Calculate the increment for the mean
+        increment = (batch_new_features - current_means) / current_counts
+
+        # Apply the increment
+        feature_array[indices] += increment
+
+    return feature_array, count_array
 
 
-def update_features(feature_list, feature_values, valid_indices):
-    # convert valid_indices to indexes
-    valid_indices = np.where(valid_indices)[0]
-    for feature, valid_index in zip(feature_values, valid_indices):
-        if feature_list[valid_index] == []:
-            feature_list[valid_index] = (feature, 1)
-        else:
-            feature_mean, count = feature_list[valid_index]
-            feature_mean, count = update_running_mean(feature_mean, count, feature)
-            feature_list[valid_index] = (feature_mean, count)
-    return feature_list
+def interpolate_missing_features(ptc_feats, ptc_feats_count, points, batch_size=128):
+    # ptc_feats: NxF - Feature array
+    # ptc_feats_count: N - Array tracking the count of each feature
+    # points: Nx3 - Array of point cloud coordinates
+
+    # Find indices of missing features (where count is 0)
+    missing_idx = np.where(ptc_feats_count == 0)[0]
+
+    # Create a KDTree for nearest neighbor search
+    tree = KDTree(points)
+
+    # Optimize batch size based on the size of missing_idx
+    # Adjust this value based on memory constraints and dataset size
+    batch_size = min(batch_size, len(missing_idx))
+
+    # Create batches of missing indices
+    batches = np.array_split(missing_idx, max(len(missing_idx) // batch_size, 1))
+
+    # Perform KNN interpolation in batches
+    for batch in tqdm(batches, total=len(batches), desc="KNN interpolation"):
+        # Find nearest neighbors (10 nearest neighbors)
+        _, idx = tree.query(points[batch], k=10)
+
+        # Filter out zero features among neighbors
+        # This has to be done in a loop because the number of neighbors is different for each point
+
+        for batch_idx, neighbor_batch in enumerate(idx):
+            nonzero_neighbors = ptc_feats[neighbor_batch, :]
+            nonzero_neighbors_mask = np.any(nonzero_neighbors != np.zeros(3), axis=-1)
+            nonzero_neighbors = nonzero_neighbors[nonzero_neighbors_mask]
+            if nonzero_neighbors.shape[0] == 0:
+                neighbors_agg = np.zeros(3)
+            else:
+                neighbors_agg = np.median(nonzero_neighbors, axis=-2)
+            ptc_feats[batch[batch_idx]] = neighbors_agg
+
+    return ptc_feats
 
 
 def process_scene(
@@ -152,9 +253,6 @@ def process_scene(
     # create videoreader and read the frames
     vr = VideoReader(movie_path, ctx=cpu(0))
 
-    # initialize list
-    ptc_feats = [[] for _ in range(len(points))]
-
     # create extractors
     if feature_type == "rgb":
         f_shape = 3
@@ -164,70 +262,52 @@ def process_scene(
         print("Loading DINO model")
         model = load_dino(dino_model_name)
 
-    # calculate features
-    for image_idx, image in tqdm(images.items(), total=len(images), desc="Processing images"):
-        # skip every nth scan
-        if image_idx % skip_scans != 0:
-            continue
+    # initialize features and set count to 0
+    ptc_feats = np.zeros((len(points), f_shape), dtype=np.float16)
+    ptc_feats_count = np.zeros((len(points), 1), dtype=np.int32)
 
-        # extract video frame
-        frame_name = image.name
-        frame = int(frame_name.split("_")[-1].split(".")[0])
+    # calculate features in batches, first skip every nth scan
+    images = list(images.values())
+    images = images[::skip_scans]
 
-        # seek the video frame
-        videoframe = vr[frame].asnumpy() / 255.0
+    batch_size = 2
+    batches = np.array_split(np.arange(len(images)), len(images) // batch_size)
+    points = np.expand_dims(points, axis=0).repeat(batch_size, axis=0)
 
-        # project the mesh on the camera and extract the rgb values from the videoframe
-        iphone_data = iphone_intrinsics[frame_name.split(".")[0]]
-        intrinsic_matrix = iphone_data["intrinsic"]
-        world_to_camera = image.world_to_camera
-        R = world_to_camera[:3, :3]
-        t = world_to_camera[:-1, -1:]
+    for batch in tqdm(batches, total=len(batches), desc="Processing images"):
+        frame_names = [images[i].name for i in batch]
+        frames = [int(frame_name.split("_")[-1].split(".")[0]) for frame_name in frame_names]
 
-        # project points
-        points_projected = project_point_cloud(points, t, R, intrinsic_matrix)
-        valid_indices = filter_points(points_projected, image_width, image_height)
+        # seek the video frames
+        videoframes = vr.get_batch(frames).asnumpy() / 255.0
+
+        # get intrinsics and extrinsics
+        intrinsic_matrices = np.array(
+            [
+                iphone_data["intrinsic"]
+                for iphone_data in [iphone_intrinsics[frame_name.split(".")[0]] for frame_name in frame_names]
+            ]
+        )
+        world_to_cameras = [images[i].world_to_camera for i in batch]
+        Rs = np.array([world_to_camera[:3, :3] for world_to_camera in world_to_cameras])
+        ts = np.array([world_to_camera[:-1, -1:] for world_to_camera in world_to_cameras])
+
+        points_projected = project_point_cloud_batch(points, ts, Rs, intrinsic_matrices)
+        valid_indices = filter_points_batch(points_projected, image_width, image_height)
 
         # extract features
         if feature_type == "rgb":
-            features, valid_indices = map_image_features_to_ptc(
-                videoframe, points_projected[valid_indices], valid_indices
-            )
+            features = map_image_features_to_filtered_ptc_batch(videoframes, points_projected, valid_indices)
         elif feature_type == "dino":
-            videoframe = torch.tensor(np.array(videoframe) / 255.0).permute(2, 0, 1).float()
-            dino_feats = get_dino_features(model, videoframe).squeeze()
-            dino_feats = rearrange(dino_feats, "c h w -> h w c").cpu().numpy()
-            features, valid_indices = map_image_features_to_ptc(
-                dino_feats, points_projected[valid_indices], valid_indices
-            )
+            with torch.cuda.amp.autocast(cache_enabled=False):
+                videoframes = torch.tensor(videoframes).permute(0, 3, 1, 2).type(torch.float16).cuda()
+                dino_feats = get_dino_features(model, videoframes)
 
-        # add the features to the pointcloud
-        update_features(ptc_feats, features, valid_indices)
+            dino_feats = rearrange(dino_feats, "b c h w -> b h w c").cpu().numpy()
+            features = map_image_features_to_filtered_ptc_batch(dino_feats, points_projected, valid_indices)
 
-    # clean the misssing features my KNN interpolation
-    missing_idx = []
-    for idx, f in tqdm(enumerate(ptc_feats), total=len(ptc_feats), desc="Cleaning features"):
-        if f != []:
-            feat, count = f
-            ptc_feats[idx] = np.array(feat)
-        else:
-            ptc_feats[idx] = np.zeros(f_shape)
-            missing_idx.append(idx)
+        update_features_batched(ptc_feats, ptc_feats_count, features, valid_indices)
 
-    ptc_feats = np.array(ptc_feats)
-
-    tree = KDTree(points)
-
-    # make batches
-    batch_size = 100
-    batches = np.array_split(missing_idx, len(missing_idx) // batch_size)
-
-    for batch in tqdm(batches, total=len(batches), desc="KNN interpolation"):
-        # find nearest neighbors
-        dist, idx = tree.query(points[batch], k=10)
-        # replace missing values with mean over nearest neighbors
-        neighbors = ptc_feats[idx]
-        neighbors_agg = np.median(neighbors, axis=1)
-        ptc_feats[batch] = neighbors_agg
-
-    np.save(target_path, ptc_feats)
+    ptc_feats = interpolate_missing_features(ptc_feats, ptc_feats_count, points[0])
+    np.nan_to_num(ptc_feats, copy=False)
+    np.save(target_path, ptc_feats.astype(np.float16))
